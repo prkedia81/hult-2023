2411e21881f099e0db5cfc5993c7862125769ab5
prannay <prannaykedia1@gmail.com>
2023-01-15 00:21:33 +0530

    updated all pages

---
+++ index.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.getElementById("menu");
+
+function openMenu() {
+  mobileMenu.classList.toggle("hidden");
+}
+
+function closeMenu() {
+  mobileMenu.classList.toggle("hidden");
+}
